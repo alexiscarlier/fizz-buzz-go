5239c532ba94379814a4126fcf68cc30c20565fe
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-05 20:11:11 +0000

    Added fizz buzz function

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,1 +1,15 @@
+package fizzbuzz
 
+import "strconv"
+
+func fizzbuzz(x int) string {
+	if x%15 == 0 {
+		return "FizzBuzz"
+	} else if x%5 == 0 {
+		return "Buzz"
+	} else if x%3 == 0 {
+		return "Fizz"
+	} else {
+		return strconv.Itoa(x)
+	}
+}
